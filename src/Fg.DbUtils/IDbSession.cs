5ab280696aa23cffa8b25929adf704da8b5a84c0
Frederik Gheysels <frederik.gheysels@codit.eu>
2022-10-31 17:22:08 +0100

    extract IDbSession interface

---
+++ src/Fg.DbUtils/IDbSession.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+
+namespace Fg.DbUtils
+{
+    internal interface IDbSession : IDbConnection
+    {
+        /// <summary>
+        /// Gets a value indicating whether the current IDbSession has an active transaction or not.
+        /// </summary>
+        bool IsInTransaction { get; }
+
+        /// <summary>
+        /// Commits the active transaction of this IDbSession.
+        /// </summary>
+        void CommitTransaction();
+
+        /// <summary>
+        /// Rollbacks the active transaction of this IDbSession.
+        /// </summary>
+        void RollbackTransaction();
+    }
+}
