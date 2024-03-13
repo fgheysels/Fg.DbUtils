5cfad4aa250f65d4d88027387d255972967b05b2
Frederik Gheysels <frederik.gheysels@codit.eu>
2024-03-13 22:16:28 +0100

    Introduce DbSessionFactory

---
+++ src/Fg.DbUtils/DbSessionFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+
+namespace Fg.DbUtils
+{
+    public class DbSessionFactory<TConnection> : IDbSessionFactory where TConnection : IDbConnection
+    {
+        private readonly Func<IDbConnection> _dbConnectionFactory;
+
+        public DbSessionFactory(Func<IDbConnection> dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public IDbSession CreateDbSession()
+        {
+            var session = new DbSession(_dbConnectionFactory());
+
+            session.Open();
+
+            return session;
+        }
+    }
+}
