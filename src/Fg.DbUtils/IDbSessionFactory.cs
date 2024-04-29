52e41413576cd63acd485e82b098a09f040c47c8
Frederik Gheysels <frederik.gheysels@codit.eu>
2024-04-30 00:50:33 +0200

    missing interface member

--- src/Fg.DbUtils/IDbSessionFactory.cs
+++ src/Fg.DbUtils/IDbSessionFactory.cs
@@ -7,5 +7,13 @@
         /// </summary>
         /// <returns>An <see cref="IDbSession"/></returns>
         IDbSession CreateDbSession();
+
+        /// <summary>
+        /// Create a new <see cref="IDbSession"/> instance
+        /// </summary>
+        /// <param name="settings">The <see cref="DbSessionSettings"/> instance that defines the settings that must
+        /// be applied to the <see cref="IDbSession"/> that is returned.</param>
+        /// <returns>An <see cref="IDbSession"/></returns>
+        IDbSession CreateDbSession(DbSessionSettings settings);
     }
 }
