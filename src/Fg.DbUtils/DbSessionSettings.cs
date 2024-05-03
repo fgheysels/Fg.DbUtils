829bf12d3dd0fdcc241433a7d0fbfafbc5a66f13
Frederik Gheysels <frederik.gheysels@codit.eu>
2024-05-03 22:15:06 +0200

    Improve DbSessionSettings creation via factory method

--- src/Fg.DbUtils/DbSessionSettings.cs
+++ src/Fg.DbUtils/DbSessionSettings.cs
@@ -4,11 +4,16 @@ namespace Fg.DbUtils
 {
     public class DbSessionSettings
     {
-        public static readonly DbSessionSettings Default = new DbSessionSettings
-        {
-            CommandTimeout = TimeSpan.FromSeconds(30)
-        };
+        public static readonly DbSessionSettings Default = DbSessionSettings.Create(TimeSpan.FromSeconds(30));
 
-        public TimeSpan CommandTimeout { get; set; } = TimeSpan.FromSeconds(30);
+        public static DbSessionSettings Create(TimeSpan commandTimeout)
+        {
+            return new DbSessionSettings()
+            {
+                CommandTimeout = commandTimeout
+            };
+        }
+
+        public TimeSpan CommandTimeout { get; private set; } = TimeSpan.FromSeconds(30);
     }
 }
