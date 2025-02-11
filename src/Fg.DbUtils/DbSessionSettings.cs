4aa93850e682d2fe15957e291053480547ab12ca
Frederik Gheysels <frederik.gheysels@codit.eu>
2025-02-11 15:46:06 +0100

    slightly improved documentation

--- src/Fg.DbUtils/DbSessionSettings.cs
+++ src/Fg.DbUtils/DbSessionSettings.cs
@@ -2,6 +2,9 @@
 
 namespace Fg.DbUtils
 {
+    /// <summary>
+    /// Defines the settings that must be applied by the DbSessionFactory when a new DbSession instance is created.
+    /// </summary>
     public class DbSessionSettings
     {
         public static readonly DbSessionSettings Default = DbSessionSettings.Create(TimeSpan.FromSeconds(30));
@@ -14,6 +17,9 @@ namespace Fg.DbUtils
             };
         }
 
+        /// <summary>
+        /// Gets or sets the wait time before terminating the SQL command that is being executed by the DbSession.
+        /// </summary>
         public TimeSpan CommandTimeout { get; private set; } = TimeSpan.FromSeconds(30);
     }
 }
